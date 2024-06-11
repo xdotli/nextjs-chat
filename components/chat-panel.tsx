de5bb297af97c4631d5181664a68bb4d0ba61677
Xiangyi Li <venusli370306@gmail.com>
2024-06-11 00:48:45 -0700

    ci: set messages to any to pass build

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -34,8 +34,27 @@ export function ChatPanel({
   const { submitUserMessage } = useActions()
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
 
-  const exampleMessages = [
-
+  const exampleMessages: any[]  = [
+    {
+      heading: 'What are the',
+      subheading: 'trending memecoins today?',
+      message: `What are the trending memecoins today?`
+    },
+    {
+      heading: 'What is the price of',
+      subheading: '$DOGE right now?',
+      message: 'What is the price of $DOGE right now?'
+    },
+    {
+      heading: 'I would like to buy',
+      subheading: '42 $DOGE',
+      message: `I would like to buy 42 $DOGE`
+    },
+    {
+      heading: 'What are some',
+      subheading: `recent events about $DOGE?`,
+      message: `What are some recent events about $DOGE?`
+    }
   ]
 
   return (
