df0c2783b3c6cd216c4f7566f573140e92828348
Xiangyi Li <venusli370306@gmail.com>
2024-06-11 00:58:31 -0700

    ci: upload model and upgrade deps

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -127,7 +127,7 @@ async function submitUserMessage(content: string) {
   let textNode: undefined | React.ReactNode
 
   const result = await streamUI({
-    model: openai('gpt-3.5-turbo'),
+    model: openai('gpt-4o	'),
     initial: <SpinnerMessage />,
     system: `\
     You are a full-stack development expert`,
